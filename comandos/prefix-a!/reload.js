5c34eb199d4fb1ff0c450006243e7b0b32380a7b
Near <37019421+NearKingBR@users.noreply.github.com>
2018-03-31 13:57:22 -0300

    Update reload.js

--- comandos/prefix-a!/reload.js
+++ comandos/prefix-a!/reload.js
@@ -7,7 +7,7 @@ const dono = "315263840268976128"
 
     let reason = args.slice(0).join(' ');
 
-    if (message.author.id === "315263840268976128" || message.author.id === '399302842688733195'){
+    if (message.author.id === "315263840268976128" || message.author.id === '399302842688733195') ||message.author.id === '401414281763094549'){
     if (reason.length < 1) return message.reply('**Diga o comando que devo reiniciar!**');
 
     delete require.cache[require.resolve(`./${args[0]}.js`)];
